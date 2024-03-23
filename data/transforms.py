e8d62a837405ec16781b843aa467775cb5e734b5
Shashaank Aiyer <saa244@cornell.edu>
2024-03-23 15:46:30 -0400

    Updated transforms.py and started model

--- data/transforms.py
+++ data/transforms.py
@@ -3,6 +3,9 @@ import torch
 import torchaudio
 from torchaudio import transforms
 from IPython.display import Audio
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader, Dataset, random_split
+import torchaudio
 
 class AudioUtil():
   # ----------------------------
@@ -106,4 +109,81 @@ class AudioUtil():
     spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
     return (spec)
   
+   # ----------------------------
+  # Augment the Spectrogram by masking out some sections of it in both the frequency
+  # dimension (ie. horizontal bars) and the time dimension (vertical bars) to prevent
+  # overfitting and to help the model generalise better. The masked sections are
+  # replaced with the mean value.
+  # ----------------------------
+  @staticmethod
+  def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+    _, n_mels, n_steps = spec.shape
+    mask_value = spec.mean()
+    aug_spec = spec
+
+    freq_mask_param = max_mask_pct * n_mels
+    for _ in range(n_freq_masks):
+      aug_spec = transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
+
+    time_mask_param = max_mask_pct * n_steps
+    for _ in range(n_time_masks):
+      aug_spec = transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
+
+    return aug_spec
+
+# ----------------------------
+# Sound Dataset
+# ----------------------------
+class SoundDS(Dataset):
+  def __init__(self, df, data_path):
+    self.df = df
+    self.data_path = str(data_path)
+    self.duration = 4000
+    self.sr = 44100
+    self.channel = 2
+    self.shift_pct = 0.4
+            
+  # ----------------------------
+  # Number of items in dataset
+  # ----------------------------
+  def __len__(self):
+    return len(self.df)    
+    
+  # ----------------------------
+  # Get i'th item in dataset
+  # ----------------------------
+  def __getitem__(self, idx):
+    # Absolute file path of the audio file - concatenate the audio directory with
+    # the relative path
+    audio_file = self.data_path + self.df.loc[idx, 'relative_path']
+    # Get the Class ID
+    class_id = self.df.loc[idx, 'classID']
+
+    aud = AudioUtil.open(audio_file)
+    # Some sounds have a higher sample rate, or fewer channels compared to the
+    # majority. So make all sounds have the same number of channels and same 
+    # sample rate. Unless the sample rate is the same, the pad_trunc will still
+    # result in arrays of different lengths, even though the sound duration is
+    # the same.
+    reaud = AudioUtil.resample(aud, self.sr)
+    rechan = AudioUtil.rechannel(reaud, self.channel)
+
+    dur_aud = AudioUtil.pad_trunc(rechan, self.duration)
+    shift_aud = AudioUtil.time_shift(dur_aud, self.shift_pct)
+    sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None)
+    aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+
+    return aug_sgram, class_id
   
+
+myds = SoundDS(df, data_path)
+
+# Random split of 80:20 between training and validation
+num_items = len(myds)
+num_train = round(num_items * 0.8)
+num_val = num_items - num_train
+train_ds, val_ds = random_split(myds, [num_train, num_val])
+
+# Create training and validation data loaders
+train_dl = torch.utils.data.DataLoader(train_ds, batch_size=16, shuffle=True)
+val_dl = torch.utils.data.DataLoader(val_ds, batch_size=16, shuffle=False)
