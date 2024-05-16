1dbcc29f80a30dcdfbe4b5fc22e4a42eec984efb
Shashaank Aiyer <saa244@cornell.edu>
2024-05-16 14:27:53 -0400

    Added live record to test.py

--- src/test.py
+++ src/test.py
@@ -2,10 +2,13 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import wavio as wv
 
+device_info = sd.query_devices(kind='input')
+input_channels = device_info['max_input_channels']
 freq = 44100
 duration = 5
+
 recording = sd.rec(int(duration * freq), 
-                   samplerate=freq, channels=2)
+                   samplerate=freq, channels=min(input_channels, 2))
 sd.wait()
 write("recording0.wav", freq, recording)
 wv.write("recording1.wav", recording, freq, sampwidth=2)
