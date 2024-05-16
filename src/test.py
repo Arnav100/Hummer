97f3ce719efc1fcd43872805997fbfd0850a6481
Shashaank Aiyer <saa244@cornell.edu>
2024-05-16 14:31:44 -0400

    Added live audio to test.py

--- src/test.py
+++ src/test.py
@@ -5,10 +5,11 @@ import wavio as wv
 device_info = sd.query_devices(kind='input')
 input_channels = device_info['max_input_channels']
 freq = 44100
-duration = 5
+duration = 10
 
 recording = sd.rec(int(duration * freq), 
                    samplerate=freq, channels=min(input_channels, 2))
+
+print("Begin Humming Now")
 sd.wait()
-write("recording0.wav", freq, recording)
-wv.write("recording1.wav", recording, freq, sampwidth=2)
+wv.write("recording.wav", recording, freq, sampwidth=2)
